e5bb81bd47f4b6a10db8cbae7d917218bda0096a
KIDZ <kidzbin@gmail.com>
2015-03-12 17:10:09 +0800

    Add SQLite3

--- AM2302/AM2302.c
+++ AM2302/AM2302.c
@@ -62,51 +62,50 @@ uint8_t FetchAM2303(_AM2302Data * AM2302Data)
  	uint8_t max=0x00,min=0xFF,mid;
  	uint8_t i,j,*pt,sum;
 
-  	pinMode(bReadPin_AM, OUTPUT);
-  	digitalWrite(bReadPin_AM, HIGH);
-  	delayMicroseconds(10);
-  	digitalWrite(bReadPin_AM, LOW);
-  	delayMicroseconds(1000);
-   	digitalWrite(bReadPin_AM, HIGH);
-  	delayMicroseconds(30);
-  	pinMode(bReadPin_AM, INPUT);
+  pinMode(bReadPin_AM, OUTPUT);
+  digitalWrite(bReadPin_AM, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(bReadPin_AM, LOW);
+  delayMicroseconds(1000);
+  digitalWrite(bReadPin_AM, HIGH);
+  delayMicroseconds(30);
+  pinMode(bReadPin_AM, INPUT);
 
 	if( Waiting(LOW)  == 255 ) return 0;
 	if( Waiting(HIGH) == 255 ) return 0;
 
-  	for( i=0 ; i< 40; i++ )
-  	{
-    	Waiting(LOW);
-    	rawus[i]=Waiting(HIGH);
- 		if(rawus[i]>max) max = rawus[i] ;
-  		if(rawus[i]<min) min = rawus[i] ;    	
-  	}
+  for( i=0 ; i< 40; i++ )
+  {
+   	Waiting(LOW);
+   	rawus[i]=Waiting(HIGH);
+ 	  if(rawus[i]>max) max = rawus[i] ;
+  	if(rawus[i]<min) min = rawus[i] ;    	
+  }
  
-  	mid = ( max + min ) / 2;
+  mid = ( max + min ) / 2;
 
-  	pt = rawus;
-
+  pt = rawus;
 	for( i=0 ; i< 5; i++ )
+  {
+  	Data[i]=0;
+  	for(j=0;j<8;j++)
   	{
-  		Data[i]=0;
-  		for(j=0;j<8;j++)
-  		{
-  			if(*pt>mid)
-  				Data[i] |= 0x01<<( 7 - (j&0x07) );		
-  			pt++;
-  		}
+  		if(*pt>mid)
+  			Data[i] |= 0x01<<( 7 - (j&0x07) );		
+  		pt++;
   	}
+  }
 
-  	// for( i=0 ; i< 5; i++ )
-  	// 	printf("%x ",Data[i]);
+  // for( i=0 ; i< 5; i++ )
+  // 	printf("%x ",Data[i]);
 
-  	// printf("\n");
+  // printf("\n");
 
-  	sum=0;
-  	for(i=0;i<4;i++)
-  		sum+=Data[i];
+  sum=0;
+  for(i=0;i<4;i++)
+  	sum+=Data[i];
 
-  	if( sum != Data[4]) return 0;
+  if( sum != Data[4]) return 0;
 
 	AM2302Data->Humidity =  (uint16_t)(Data[0]<<8 | Data[1]);
 	AM2302Data->Room     =  (uint16_t)(Data[2]<<8 | Data[3]);
--- TempMod.c
+++ TempMod.c
@@ -2,11 +2,19 @@
 #include <stdint.h>
 #include <wiringPi.h>
 #include <time.h>
+#include <sqlite3.h>
 #include "DS1820/DS1820.h"
 #include "AM2302/AM2302.h"
 
 #define DS_PIN 0
 #define AM_PIN 1
+
+// #create table Home(ID INTEGER PRIMARY KEY AUTOINCREMENT, 
+// #                    TimeStamp INTEGER DEFAULT (datetime('now','localtime')),
+// #		     		DS_Temp REAL ,
+// # 	             	AM_Humi REAL,
+// # 	             	AM_Room REAL);
+
 
 int main(void)
 {
@@ -15,17 +23,29 @@ int main(void)
  	uint8_t  TempStatus;
  	time_t now;
  	struct tm *local;
+ 	sqlite3 *db;
+ 	int rc;
+ 	char *errMsg = NULL;
+ 	char InsertSQL[128];
+
+ 	rc = sqlite3_open("HomeTemp.db", &db);
+ 	
+ 	if(rc) 
+ 	{
+ 		printf("Error\n");
+ 		return 0;
+ 	}
 
 	InitDS1820(DS_PIN);
 	InitAM2302(AM_PIN);
 
   	if(wiringPiSetup() == -1)
     	return -1;
-
 	do
 	{
 		time(&now);
 		local = localtime(&now);
+
 		TempStatus = 0x00;
 		if(FetchDS(&DSTemp))
 			TempStatus |= 0x01;
@@ -36,14 +56,18 @@ int main(void)
 		if(TempStatus == 0x03)
 		{
 			printf("DS1820 Temp = %.2fC , AM2302 Room=%.2fC,Humi=%.2f%c  %2d:%2d:%2d\n" ,(float)DSTemp*0.0625 , (float)AM2302Data.Room/10 ,(float)AM2302Data.Humidity/10,37,local->tm_hour,local->tm_min,local->tm_sec );
-			delay(1000);
+			sprintf(InsertSQL,"INSERT INTO Home (DS_Temp,AM_Humi,AM_Room) VALUES(%.2f,%.1f,%.1f);" , (float)DSTemp*0.0625,(float)AM2302Data.Humidity/10,(float)AM2302Data.Room/10);
+			//printf("%s\n",InsertSQL);
+			rc = sqlite3_exec(db, InsertSQL, 0, 0, &errMsg);
+			delay(3000);
 		}
 		else
 		{
-			delay(300);
+			delay(750);
 		}
-
 	}while(1);
+
+	sqlite3_close(db); 
 
   	return 0x00;
 }
